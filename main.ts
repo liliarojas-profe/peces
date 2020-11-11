53b1a10054927be306f361543a3381c57f9dee8e
liliarojas-profe <68862541+liliarojas-profe@users.noreply.github.com>
2020-11-11 14:58:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+enum RadioMessage {
+    message1 = 49434
+}
+input.onButtonPressed(Button.A, function () {
+    pesca = randint(1, 10)
+    radio.sendNumber(pesca)
+    if (pesca > 0) {
+        basic.showLeds(`
+            # . # # .
+            # # # # #
+            # # # # #
+            # . # # .
+            . . . . .
+            `)
+        basic.showNumber(pesca)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    pesca = randint(0, 1)
+    radio.sendNumber(pesca)
+    if (pesca > 0) {
+        basic.showLeds(`
+            # . # # .
+            # # # # #
+            # # # # #
+            # . # # .
+            . . . . .
+            `)
+        basic.showNumber(pesca)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+let pesca = 0
+radio.setGroup(1)
+pesca = 0
